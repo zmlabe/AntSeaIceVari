7aea75ea2c545f31e65aacc6bf11b8bd9a7203ba
Zachary Labe <zlabe@uci.edu>
2019-08-12 10:13:27 -0700

    First commit

---
+++ Scripts/__init__.py
@@ -0,0 +1,8 @@
+'''
+The Python repository "AntSeaIceVari" contains the scripts, data,
+figures, and text for 2 AMIP-style simulations following the
+PAMIP protocol for 100 year periods. The models are SC-WACCM4
+and E3SM. We are interested in the internal variability of the 
+atmospheric response to Antarctic sea-ice loss.
+Author: Zachary Labe (zlabe@uci.edu)
+'''
